28df5d69b69699e43c3df81f0e7c0281d9134c0d
M-racle <108946729+M-racle@users.noreply.github.com>
2023-12-22 21:13:54 +0300

    TechCareer Bootcamp Odev-1

---
+++ TechCareerWebApiTutorial/Controllers/CountriesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TechCareerWebApiTutorial.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase {
+
+        private readonly string[] countries = {
+    "Türkiye", "Almanya", "Fransa", "ABD", "İngiltere", "Yunanistan", "Bulgaristan", "İsveç", "İsviçre", "Çekya", "Katar", "Rusya", "Özbekistan", "Türkmenistan", "Moğolistan", "Meksika", "Kanada", "İzlanda", "Irak", "Suriye"
+        };
+
+        [HttpGet]
+        public IActionResult GetAllCountries() {
+            var result = new {
+                Countries = countries,
+                kayitSayisi = countries.Length
+            };
+
+            return Ok(result);
+        }
+
+        [HttpGet("{sayi}")]
+        public IActionResult GetCountries(int sayi) {
+            var secilen = new string[sayi];
+            Array.Copy(countries, secilen, sayi);
+
+            return Ok(secilen);
+        }
+
+    }
+}
